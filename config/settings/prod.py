829e26fa88f54cd768e16be68739c862172b7a84
Bin <phamon0@gmail.com>
2021-03-16 14:32:10 +0900

    데이터베이스 구축

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['3.35.191.255']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASEs = {
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'g6=cbzwNeooAMk+nR:5jD&9W&`qCf?3#',
+        'HOST': 'ls-83e0d565962a60e921f1654524ab43fc552c14bd.chnosgxs5ver.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
